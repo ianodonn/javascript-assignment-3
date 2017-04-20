3d3cdc41a82da3c77479e1a10360119e01eb8a8d
Ian O'Donnell <iano@id.iit.edu>
2017-04-20 17:33:42 -0500

    Init commit Created divs 1 through 3 Created a ray creating while loop

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // Do your work in this file.
+var div1 = document.createElement("div");
+div1.className = "view";
+document.body.appendChild(div1);
 
+var div2 = document.createElement("div");
+div2.className = "grass";
+div1.appendChild(div2);
+
+var div3 = document.createElement("div");
+div3.className = "sun";
+div1.appendChild(div3);
+
+var i = 1;
+
+while (i<6){
+    var div = document.createElement("div");
+    div.className = "ray-"+i;
+    div3.appendChild(div);
+    i++;
+}
